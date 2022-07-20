6f1b13c49e4aa7a872255e213aa856e11ea04167
Kadir-Yildiz <105437364+Kadir-Yildiz@users.noreply.github.com>
2022-07-20 10:02:24 +0300

    GelirGiderFormTasarimi

---
+++ BarkodluSatisProgrami/fGelirGider.Designer.cs
@@ -0,0 +1,200 @@
+﻿namespace BarkodluSatisProgrami
+{
+    partial class fGelirGider
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lStandart1 = new BarkodluSatisProgrami.lStandart();
+            this.lStandart2 = new BarkodluSatisProgrami.lStandart();
+            this.cmbIslemTuru = new System.Windows.Forms.ComboBox();
+            this.tNumeric1 = new BarkodluSatisProgrami.tNumeric();
+            this.tNumeric2 = new BarkodluSatisProgrami.tNumeric();
+            this.lStandart3 = new BarkodluSatisProgrami.lStandart();
+            this.lStandart4 = new BarkodluSatisProgrami.lStandart();
+            this.tStandart1 = new BarkodluSatisProgrami.tStandart();
+            this.lStandart5 = new BarkodluSatisProgrami.lStandart();
+            this.dtBaslangic = new System.Windows.Forms.DateTimePicker();
+            this.lStandart6 = new BarkodluSatisProgrami.lStandart();
+            this.SuspendLayout();
+            // 
+            // lStandart1
+            // 
+            this.lStandart1.AutoSize = true;
+            this.lStandart1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.lStandart1.ForeColor = System.Drawing.Color.DarkCyan;
+            this.lStandart1.Location = new System.Drawing.Point(36, 26);
+            this.lStandart1.Name = "lStandart1";
+            this.lStandart1.Size = new System.Drawing.Size(101, 25);
+            this.lStandart1.TabIndex = 0;
+            this.lStandart1.Text = "lStandart1";
+            // 
+            // lStandart2
+            // 
+            this.lStandart2.AutoSize = true;
+            this.lStandart2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.lStandart2.ForeColor = System.Drawing.Color.DarkCyan;
+            this.lStandart2.Location = new System.Drawing.Point(36, 74);
+            this.lStandart2.Name = "lStandart2";
+            this.lStandart2.Size = new System.Drawing.Size(123, 25);
+            this.lStandart2.TabIndex = 1;
+            this.lStandart2.Text = "Ödeme Türü";
+            // 
+            // cmbIslemTuru
+            // 
+            this.cmbIslemTuru.BackColor = System.Drawing.SystemColors.InactiveBorder;
+            this.cmbIslemTuru.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cmbIslemTuru.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.cmbIslemTuru.FormattingEnabled = true;
+            this.cmbIslemTuru.Items.AddRange(new object[] {
+            "Stok Durumu",
+            "Stok Giriş İzleme"});
+            this.cmbIslemTuru.Location = new System.Drawing.Point(41, 113);
+            this.cmbIslemTuru.Name = "cmbIslemTuru";
+            this.cmbIslemTuru.Size = new System.Drawing.Size(274, 30);
+            this.cmbIslemTuru.TabIndex = 2;
+            // 
+            // tNumeric1
+            // 
+            this.tNumeric1.BackColor = System.Drawing.Color.White;
+            this.tNumeric1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.tNumeric1.Location = new System.Drawing.Point(41, 216);
+            this.tNumeric1.Name = "tNumeric1";
+            this.tNumeric1.Size = new System.Drawing.Size(115, 30);
+            this.tNumeric1.TabIndex = 3;
+            this.tNumeric1.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            // 
+            // tNumeric2
+            // 
+            this.tNumeric2.BackColor = System.Drawing.Color.White;
+            this.tNumeric2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.tNumeric2.Location = new System.Drawing.Point(200, 216);
+            this.tNumeric2.Name = "tNumeric2";
+            this.tNumeric2.Size = new System.Drawing.Size(115, 30);
+            this.tNumeric2.TabIndex = 4;
+            this.tNumeric2.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            // 
+            // lStandart3
+            // 
+            this.lStandart3.AutoSize = true;
+            this.lStandart3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.lStandart3.ForeColor = System.Drawing.Color.DarkCyan;
+            this.lStandart3.Location = new System.Drawing.Point(36, 172);
+            this.lStandart3.Name = "lStandart3";
+            this.lStandart3.Size = new System.Drawing.Size(56, 25);
+            this.lStandart3.TabIndex = 5;
+            this.lStandart3.Text = "Nakit";
+            // 
+            // lStandart4
+            // 
+            this.lStandart4.AutoSize = true;
+            this.lStandart4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.lStandart4.ForeColor = System.Drawing.Color.DarkCyan;
+            this.lStandart4.Location = new System.Drawing.Point(195, 172);
+            this.lStandart4.Name = "lStandart4";
+            this.lStandart4.Size = new System.Drawing.Size(48, 25);
+            this.lStandart4.TabIndex = 6;
+            this.lStandart4.Text = "Kart";
+            // 
+            // tStandart1
+            // 
+            this.tStandart1.BackColor = System.Drawing.Color.White;
+            this.tStandart1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.tStandart1.Location = new System.Drawing.Point(41, 289);
+            this.tStandart1.Multiline = true;
+            this.tStandart1.Name = "tStandart1";
+            this.tStandart1.Size = new System.Drawing.Size(250, 151);
+            this.tStandart1.TabIndex = 7;
+            // 
+            // lStandart5
+            // 
+            this.lStandart5.AutoSize = true;
+            this.lStandart5.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.lStandart5.ForeColor = System.Drawing.Color.DarkCyan;
+            this.lStandart5.Location = new System.Drawing.Point(36, 261);
+            this.lStandart5.Name = "lStandart5";
+            this.lStandart5.Size = new System.Drawing.Size(92, 25);
+            this.lStandart5.TabIndex = 8;
+            this.lStandart5.Text = "Açıklama";
+            // 
+            // dtBaslangic
+            // 
+            this.dtBaslangic.CalendarMonthBackground = System.Drawing.Color.Wheat;
+            this.dtBaslangic.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.dtBaslangic.Location = new System.Drawing.Point(41, 493);
+            this.dtBaslangic.Name = "dtBaslangic";
+            this.dtBaslangic.Size = new System.Drawing.Size(229, 28);
+            this.dtBaslangic.TabIndex = 13;
+            // 
+            // lStandart6
+            // 
+            this.lStandart6.AutoSize = true;
+            this.lStandart6.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.lStandart6.ForeColor = System.Drawing.Color.DarkCyan;
+            this.lStandart6.Location = new System.Drawing.Point(36, 465);
+            this.lStandart6.Name = "lStandart6";
+            this.lStandart6.Size = new System.Drawing.Size(57, 25);
+            this.lStandart6.TabIndex = 12;
+            this.lStandart6.Text = "Tarih";
+            // 
+            // fGelirGider
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(815, 758);
+            this.Controls.Add(this.dtBaslangic);
+            this.Controls.Add(this.lStandart6);
+            this.Controls.Add(this.lStandart5);
+            this.Controls.Add(this.tStandart1);
+            this.Controls.Add(this.lStandart4);
+            this.Controls.Add(this.lStandart3);
+            this.Controls.Add(this.tNumeric2);
+            this.Controls.Add(this.tNumeric1);
+            this.Controls.Add(this.cmbIslemTuru);
+            this.Controls.Add(this.lStandart2);
+            this.Controls.Add(this.lStandart1);
+            this.Name = "fGelirGider";
+            this.Text = "Gelir-Gider İşlemleri";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private lStandart lStandart1;
+        private lStandart lStandart2;
+        private System.Windows.Forms.ComboBox cmbIslemTuru;
+        private tNumeric tNumeric1;
+        private tNumeric tNumeric2;
+        private lStandart lStandart3;
+        private lStandart lStandart4;
+        private tStandart tStandart1;
+        private lStandart lStandart5;
+        private System.Windows.Forms.DateTimePicker dtBaslangic;
+        private lStandart lStandart6;
+    }
+}
